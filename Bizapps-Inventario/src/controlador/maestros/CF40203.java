de7bb89e16578311fb6a1b6d5ffc4713658e9d73
vanedunob <vanedunob@gmail.com>
2014-04-21 19:56:30 +0000

    F00021. F0006, F40203

---
+++ Bizapps-Inventario/src/controlador/maestros/CF40203.java
@@ -0,0 +1,85 @@
+package controlador.maestros;
+
+import java.io.IOException;
+
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Div;
+
+import componentes.Botonera;
+
+public class CF40203 extends CGenerico {
+
+	@Wire
+	private Div divVF40203;
+	
+	@Wire
+	private Div botoneraF40203;
+
+	@Override
+	public void inicializar() throws IOException {
+		// TODO Auto-generated method stub
+
+		Botonera botonera = new Botonera() {
+
+			@Override
+			public void seleccionar() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void salir() {
+				cerrarVentana(divVF40203, "Sistema");
+
+			}
+
+			@Override
+			public void reporte() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void limpiar() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void guardar() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void eliminar() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void buscar() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void ayuda() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void annadir() {
+				// TODO Auto-generated method stub
+
+			}
+		};
+//		botonera.getChildren().get(3).setVisible(false);
+//		botonera.getChildren().get(5).setVisible(false);
+		botoneraF40203.appendChild(botonera);
+		
+
+	}
+
+}
