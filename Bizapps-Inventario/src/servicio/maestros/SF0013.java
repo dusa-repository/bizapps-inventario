a32c6bf1571b2625cb420fbb5f4d3f818b1a852c
linkin9014 <linkin9014@gmail.com>
2014-04-25 22:27:47 +0000

    Crud F0013, faltan pruebas. Lista autoadatable al contenido

--- Bizapps-Inventario/src/servicio/maestros/SF0013.java
+++ Bizapps-Inventario/src/servicio/maestros/SF0013.java
@@ -22,4 +22,16 @@ public class SF0013 {
 	public F0013 buscar(String txtCCCRCDF0010) {
 		return f0013DAO.findOne(txtCCCRCDF0010);
 	}
+
+	public void guardar(F0013 f0013) {
+		f0013DAO.saveAndFlush(f0013);
+	}
+
+	public void eliminarVarios(List<F0013> eliminarLista) {
+		f0013DAO.delete(eliminarLista);
+	}
+
+	public void eliminarUno(String clave) {
+		f0013DAO.delete(clave);
+	}
 }
