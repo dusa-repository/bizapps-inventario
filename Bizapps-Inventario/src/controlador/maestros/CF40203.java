0e483edbb69bf913d674942d9964b1ca5ec115ac
vanedunob@gmail.com <vanedunob@gmail.com@4efadf6c-aee0-ddf4-0d49-3386b99f6c9a>
2014-04-27 17:04:38 +0000

    CRUD F40203. faltan pruebas

--- Bizapps-Inventario/src/controlador/maestros/CF40203.java
+++ Bizapps-Inventario/src/controlador/maestros/CF40203.java
@@ -6,15 +6,47 @@ import java.util.List;
 
 import modelo.maestros.F40203;
 import modelo.maestros.F40203;
+import modelo.pk.F40203PK;
+import modelo.pk.F40203PK;
 
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Div;
+import org.zkoss.zul.Groupbox;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Textbox;
 
 import componentes.Botonera;
 import componentes.Catalogo;
+import componentes.Mensaje;
 
 public class CF40203 extends CGenerico {
 
+	@Wire
+	private Textbox txtDCTOF40203;
+	@Wire
+	private Textbox txtLNTYF40203;
+	@Wire
+	private Textbox txtNSF40203;
+	@Wire
+	private Textbox txtTRTYF40203;
+	@Wire
+	private Textbox txtDSC1F40203;
+	@Wire
+	private Textbox txtNXTRF40203;
+	@Wire
+	private Textbox txtA1TRF40203;
+	@Wire
+	private Textbox txtA2TRF40203;
+	@Wire
+	private Textbox txtA3TRF40203;
+	@Wire
+	private Textbox txtA4TRF40203;
+	@Wire
+	private Textbox txtA5TRF40203;
+	@Wire
+	private Textbox txtWRTHF40203;
 	@Wire
 	private Div divVF40203;
 	@Wire
@@ -22,16 +54,47 @@ public class CF40203 extends CGenerico {
 	@Wire
 	private Div catalogoF40203;
 	Catalogo<F40203> catalogo;
+	@Wire
+	private Groupbox gpxDatos;
+	@Wire
+	private Groupbox gpxRegistro;
+
+	Botonera botonera;
+	F40203PK clave = null;
 	@Override
 	public void inicializar() throws IOException {
-		// TODO Auto-generated method stub
-
-		Botonera botonera = new Botonera() {
+		
+		txtDCTOF40203.setFocus(true);
+		mostrarCatalogo();
+		botonera = new Botonera()  {
 
 			@Override
 			public void seleccionar() {
-				// TODO Auto-generated method stub
-
+				if (validarSeleccion()) {
+					if (catalogo.obtenerSeleccionados().size() == 1) {
+						mostrarBotones(false);
+						abrirRegistro();
+						F40203 f40 = catalogo.objetoSeleccionadoDelCatalogo();
+						clave = f40.getId();
+						txtDCTOF40203.setValue(f40.getId().getFsdcto());
+						txtDCTOF40203.setDisabled(true);
+						txtLNTYF40203.setValue(f40.getId().getFslnty());
+						txtLNTYF40203.setDisabled(true);
+						txtTRTYF40203.setValue(f40.getId().getFslnty());
+						txtTRTYF40203.setDisabled(true);
+						//txtNSF40203.setValue(f40.);
+						txtDSC1F40203.setValue(f40.getFsstds());
+						txtNXTRF40203.setValue(f40.getFsnxtr());
+						txtA1TRF40203.setValue(f40.getFsa1tr());
+						txtA2TRF40203.setValue(f40.getFsa2tr());
+						txtA3TRF40203.setValue(f40.getFsa3tr());
+						txtA4TRF40203.setValue(f40.getFsa4tr());
+						txtA5TRF40203.setValue(f40.getFsa5tr());
+						txtWRTHF40203.setValue(f40.getFswrth());
+						txtDCTOF40203.setFocus(true);
+					} else
+						msj.mensajeAlerta(Mensaje.editarSoloUno);
+				}
 			}
 
 			@Override
@@ -48,19 +111,108 @@ public class CF40203 extends CGenerico {
 
 			@Override
 			public void limpiar() {
-				// TODO Auto-generated method stub
+				mostrarBotones(false);
+				limpiarCampos();
+				habilitarTextClave();
+				clave = null;
 
 			}
 
 			@Override
 			public void guardar() {
-				// TODO Auto-generated method stub
+				boolean guardar = true;
+				if (clave == null)
+					guardar = validar();
+				if (guardar) {
+					String dcto = txtDCTOF40203.getValue();
+					String lnty = txtLNTYF40203.getValue();
+					String trty = txtTRTYF40203.getValue();
+					String ns = txtNSF40203.getValue();
+					String dsc1 = txtDSC1F40203.getValue();
+					String nxtr = txtNXTRF40203.getValue();
+					String a1tr = txtA1TRF40203.getValue();
+					String a2tr = txtA2TRF40203.getValue();
+					String a3tr = txtA3TRF40203.getValue();
+					String a4tr = txtA4TRF40203.getValue();
+					String a5tr = txtA5TRF40203.getValue();
+					String wrth = txtWRTHF40203.getValue();
+					F40203PK clave = new F40203PK();
+					clave.setFsdcto(dcto);
+					clave.setFslnty(lnty);
+					clave.setFstrty(trty);
+					F40203 f40203 = new F40203();
+					f40203.setId(clave);
+					//f40203
+					f40203.setFsstds(dsc1);
+					f40203.setFsnxtr(nxtr);
+					f40203.setFsa1tr(a1tr);
+					f40203.setFsa2tr(a2tr);
+					f40203.setFsa3tr(a3tr);
+					f40203.setFsa4tr(a4tr);
+					f40203.setFsa5tr(a5tr);
+					f40203.setFswrth(wrth);
+					servicioF40203.guardar(f40203);
+					msj.mensajeInformacion(Mensaje.guardado);
+					limpiar();
+					catalogo.actualizarLista(servicioF40203
+							.buscarTodosOrdenados());
+				}
 
 			}
 
 			@Override
 			public void eliminar() {
-				// TODO Auto-generated method stub
+				if (gpxDatos.isOpen()) {
+					/* Elimina Varios Registros */
+					if (validarSeleccion()) {
+						final List<F40203> eliminarLista = catalogo
+								.obtenerSeleccionados();
+						Messagebox
+								.show("¿Desea Eliminar los "
+										+ eliminarLista.size() + " Registros?",
+										"Alerta",
+										Messagebox.OK | Messagebox.CANCEL,
+										Messagebox.QUESTION,
+										new org.zkoss.zk.ui.event.EventListener<Event>() {
+											public void onEvent(Event evt)
+													throws InterruptedException {
+												if (evt.getName()
+														.equals("onOK")) {
+													servicioF40203
+															.eliminarVarios(eliminarLista);
+													msj.mensajeInformacion(Mensaje.eliminado);
+													catalogo.actualizarLista(servicioF40203
+															.buscarTodosOrdenados());
+												}
+											}
+										});
+					}
+				} else {
+					/* Elimina un solo registro */
+					if (clave != null) {
+						Messagebox
+								.show(Mensaje.deseaEliminar,
+										"Alerta",
+										Messagebox.OK | Messagebox.CANCEL,
+										Messagebox.QUESTION,
+										new org.zkoss.zk.ui.event.EventListener<Event>() {
+											public void onEvent(Event evt)
+													throws InterruptedException {
+												if (evt.getName()
+														.equals("onOK")) {
+													servicioF40203
+															.eliminarUno(clave);
+													msj.mensajeInformacion(Mensaje.eliminado);
+													limpiar();
+													catalogo.actualizarLista(servicioF40203
+															.buscarTodosOrdenados());
+												}
+											}
+										});
+					} else
+						msj.mensajeAlerta(Mensaje.noSeleccionoRegistro);
+				}
+
 
 			}
 
@@ -78,18 +230,166 @@ public class CF40203 extends CGenerico {
 
 			@Override
 			public void annadir() {
-				// TODO Auto-generated method stub
+				abrirRegistro();
+				mostrarBotones(false);
 
 			}
 		};
-//		botonera.getChildren().get(3).setVisible(false);
-//		botonera.getChildren().get(5).setVisible(false);
+		botonera.getChildren().get(3).setVisible(false);
+		botonera.getChildren().get(5).setVisible(false);
 		botoneraF40203.appendChild(botonera);
-		mostrarCatalogo (); 
 
 	}
+	
+	public void mostrarBotones(boolean bol) {
+		botonera.getChildren().get(1).setVisible(bol);
+		botonera.getChildren().get(2).setVisible(bol);
+		botonera.getChildren().get(6).setVisible(bol);
+		botonera.getChildren().get(0).setVisible(bol);
+		botonera.getChildren().get(3).setVisible(!bol);
+		botonera.getChildren().get(5).setVisible(!bol);
+	}
+	
+	public void limpiarCampos() {
+		clave = null;
+		txtDCTOF40203.setValue("");
+		txtLNTYF40203.setValue("");
+		txtNSF40203.setValue("");
+		txtTRTYF40203.setValue("");
+		txtDSC1F40203.setValue("");
+		txtNXTRF40203.setValue("");
+		txtA1TRF40203.setValue("");
+		txtA2TRF40203.setValue("");
+		txtA3TRF40203.setValue("");
+		txtA4TRF40203.setValue("");
+		txtA5TRF40203.setValue("");
+		txtWRTHF40203.setValue("");
+		txtDCTOF40203.setFocus(true);
+	}
+
+	public void habilitarTextClave() {
+		if (txtDCTOF40203.isDisabled())
+			txtDCTOF40203.setDisabled(false);
+		if (txtLNTYF40203.isDisabled())
+			txtLNTYF40203.setDisabled(false);
+		if (txtTRTYF40203.isDisabled())
+			txtTRTYF40203.setDisabled(false);
+	}
+	
+	public boolean validarSeleccion() {
+		List<F40203> seleccionados = catalogo.obtenerSeleccionados();
+		if (seleccionados == null) {
+			msj.mensajeAlerta(Mensaje.noHayRegistros);
+			return false;
+		} else {
+			if (seleccionados.isEmpty()) {
+				msj.mensajeAlerta(Mensaje.noSeleccionoItem);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+	
+	protected boolean validar() {
+		if (claveDCTOExiste()) {
+			return false;
+		} else {
+			if (!camposLLenos()) {
+				msj.mensajeAlerta(Mensaje.camposVacios);
+				return false;
+			} else
+				return true;
+		}
+	}
+
+	@Listen("onChange = #txtDCTOF40203")
+	public boolean claveDCTOExiste() {
+		if (servicioF40203.buscar(txtDCTOF40203.getValue(), txtLNTYF40203.getValue(), txtTRTYF40203.getValue()) != null) {
+			msj.mensajeAlerta(Mensaje.claveUsada);
+			txtDCTOF40203.setFocus(true);
+			return true;
+		} else
+			return false;
+	}
+
+
+	public boolean camposLLenos() {
+		if (txtDCTOF40203.getText().compareTo("") == 0
+				|| txtLNTYF40203.getText().compareTo("") == 0
+				|| txtNSF40203.getText().compareTo("") == 0
+				|| txtTRTYF40203.getText().compareTo("") == 0
+				|| txtDSC1F40203.getText().compareTo("") == 0
+				|| txtNXTRF40203.getText().compareTo("") == 0
+				|| txtA1TRF40203.getText().compareTo("") == 0
+				|| txtA2TRF40203.getText().compareTo("") == 0
+				|| txtA3TRF40203.getText().compareTo("") == 0
+				|| txtA4TRF40203.getText().compareTo("") == 0
+				|| txtA5TRF40203.getText().compareTo("") == 0
+				|| txtWRTHF40203.getText().compareTo("") == 0) {
+			return false;
+		} else
+			return true;
+	}
+	
+	
+	@Listen("onClick = #gpxRegistro")
+	public void abrirRegistro() {
+		gpxDatos.setOpen(false);
+		gpxRegistro.setOpen(true);
+		mostrarBotones(false);
+	}
+	
+	public boolean camposEditando() {
+		if (txtDCTOF40203.getText().compareTo("") != 0
+				|| txtLNTYF40203.getText().compareTo("") != 0
+				|| txtNSF40203.getText().compareTo("") != 0
+				|| txtTRTYF40203.getText().compareTo("") != 0
+				|| txtDSC1F40203.getText().compareTo("") != 0
+				|| txtNXTRF40203.getText().compareTo("") != 0
+				|| txtA1TRF40203.getText().compareTo("") != 0
+				|| txtA2TRF40203.getText().compareTo("") != 0
+				|| txtA3TRF40203.getText().compareTo("") != 0
+				|| txtA4TRF40203.getText().compareTo("") != 0
+				|| txtA5TRF40203.getText().compareTo("") != 0
+				|| txtWRTHF40203.getText().compareTo("") != 0) {
+			return true;
+		} else
+			return false;
+	}
+	
+	@Listen("onOpen = #gpxDatos")
+	public void abrirCatalogo() {
+		gpxDatos.setOpen(false);
+		if (camposEditando()) {
+			Messagebox.show(Mensaje.estaEditando, "Alerta", Messagebox.YES
+					| Messagebox.NO, Messagebox.QUESTION,
+					new org.zkoss.zk.ui.event.EventListener<Event>() {
+						public void onEvent(Event evt)
+								throws InterruptedException {
+							if (evt.getName().equals("onYes")) {
+								gpxDatos.setOpen(false);
+								gpxRegistro.setOpen(true);
+							} else {
+								if (evt.getName().equals("onNo")) {
+									gpxDatos.setOpen(true);
+									gpxRegistro.setOpen(false);
+									limpiarCampos();
+									habilitarTextClave();
+									mostrarBotones(true);
+								}
+							}
+						}
+					});
+		} else {
+			gpxDatos.setOpen(true);
+			gpxRegistro.setOpen(false);
+			mostrarBotones(true);
+		}
+	}
+
 	public void mostrarCatalogo() {
-		final List<F40203> actividades = servicioF40203.buscarTodosContado();
+		final List<F40203> actividades = servicioF40203.buscarTodosOrdenados();
 		catalogo = new Catalogo<F40203>(catalogoF40203, "F40203", actividades, 
 				"Tipo Orden", "Tipo Línea", "Último estado", "Descripción", 
 				"Est sig", "Otros 1", "Otros 2", "Otros 3", "Otros 4" , "Otros 5", "LM (Y/M)") {
--- Bizapps-Inventario/src/servicio/maestros/SF40203.java
+++ Bizapps-Inventario/src/servicio/maestros/SF40203.java
@@ -19,8 +19,32 @@ public class SF40203 {
 	@Autowired
 	private IF40203DAO f40203DAO;
 
-	public List<F40203> buscarTodosContado() {
-		return f40203DAO.findAll();
+	public List<F40203> buscarTodosOrdenados() {
+		return f40203DAO.findAllOrderByIdfsdcto();
+	}
+	
+	public F40203 buscar(String value, String value2, String value3) {
+		F40203PK clave = new F40203PK();
+		clave.setFsdcto(value);
+		clave.setFstrty(value2);
+		clave.setFslnty(value3);
+		return f40203DAO.findOne(clave);
 	}
 
+
+	public void guardar(F40203 f40203) {
+		f40203DAO.save(f40203);
+	}
+
+	public void eliminarVarios(List<F40203> eliminar) {
+		f40203DAO.delete(eliminar);
+	}
+
+	public void eliminarUno(F40203PK clave) {
+		f40203DAO.delete(clave);
+	}
+
+	public List<F40203> buscarSY(String value) {
+		return f40203DAO.findByIdFsdcto(value);
+	}
 }
