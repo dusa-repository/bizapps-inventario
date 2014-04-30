217fe24c939d6dedf28dfc9f1938428959fabe1f
vanedunob <vanedunob@gmail.com>
2014-04-30 03:02:17 +0000

    Añadiendo metodos a F00021

--- Bizapps-Inventario/src/interfacedao/maestros/IF00021DAO.java
+++ Bizapps-Inventario/src/interfacedao/maestros/IF00021DAO.java
@@ -7,7 +7,18 @@ import modelo.pk.F00021PK;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IF00021DAO extends JpaRepository<F00021, F00021PK> {
+	
+	@Query("Select f from F00021 f order by f.id.nlkco asc, f.id.nldct asc, f.id.nlctry asc, f.id.nlfy asc")
+	List<F00021> findAllOrderByIdNlkco();
+
+	@Query("Select f.nln001 from F00021 f where f.id.nlkco =:value and f.id.nldct =:value2 ")
+	public double Numero(
+	@Param("value") String a,
+	@Param("value2") String b);
+
+	List<F00021> findByIdNlkco(String value);
 
 }
--- Bizapps-Inventario/src/servicio/maestros/SF00021.java
+++ Bizapps-Inventario/src/servicio/maestros/SF00021.java
@@ -5,7 +5,9 @@ import java.util.List;
 import interfacedao.maestros.IF00021DAO;
 
 import modelo.maestros.F00021;
+import modelo.maestros.F40203;
 import modelo.pk.F00021PK;
+import modelo.pk.F40203PK;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +21,35 @@ public class SF00021 {
 	@Autowired
 	private IF00021DAO f00021DAO;
 
-	public List<F00021> buscarTodosContado() {
-		return f00021DAO.findAll();
+	public List<F00021> buscarTodosOrdenados() {
+		return f00021DAO.findAllOrderByIdNlkco();
+	}
+	
+	public F00021 buscar(String value, String value2) {
+		F00021PK clave = new F00021PK();
+		clave.setNlkco(value);
+		clave.setNldct(value2);
+		return f00021DAO.findOne(clave);
+	}
+	
+	public double Numero(String value, String value2) {
+			return f00021DAO.Numero(value, value2);
 	}
 
+
+	public void guardar(F00021 f00021) {
+		f00021DAO.save(f00021);
+	}
+
+	public void eliminarVarios(List<F00021> eliminar) {
+		f00021DAO.delete(eliminar);
+	}
+
+	public void eliminarUno(F00021PK clave) {
+		f00021DAO.delete(clave);
+	}
+
+	public List<F00021> buscarSY(String value) {
+		return f00021DAO.findByIdNlkco(value);
+	}
 }
