deb7f20de43980ed1508122380dbdc135783d0e6
eanzola23 <eanzola23@gmail.com>
2014-06-01 23:52:05 +0000

    Implementacion de metodos en el CF4101 y creacion de servicios en SF4101

--- Bizapps-Inventario/src/servicio/maestros/SF4101.java
+++ Bizapps-Inventario/src/servicio/maestros/SF4101.java
@@ -3,7 +3,9 @@ package servicio.maestros;
 import java.util.List;
 
 import interfacedao.maestros.IF4101DAO;
+import modelo.maestros.F0008;
 import modelo.maestros.F4101;
+import modelo.pk.F0008PK;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,18 @@ public class SF4101 {
 	@Autowired
 	private IF4101DAO f4101DAO;
 
+	public void guardar(F4101 f4101) {
+		f4101DAO.save(f4101);
+	}
+	
+	public void eliminarVarios(List<F4101> eliminar) {
+		f4101DAO.delete(eliminar);
+	}
+
+	public void eliminarUno(double umitm) {
+		f4101DAO.delete(umitm);
+	}
+	
 	public F4101 buscar(double umitm) {
 		return f4101DAO.findOne(umitm);
 	}
