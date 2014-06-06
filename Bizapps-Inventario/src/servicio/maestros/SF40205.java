fba5ab1dd700e0fcda49079a87f980d855b6087d
linkin9014 <linkin9014@4efadf6c-aee0-ddf4-0d49-3386b99f6c9a>
2014-06-06 17:38:02 +0000

    Listo crud F40205. Falta mejorar la vista

--- Bizapps-Inventario/src/servicio/maestros/SF40205.java
+++ Bizapps-Inventario/src/servicio/maestros/SF40205.java
@@ -1,6 +1,10 @@
 package servicio.maestros;
 
+import java.util.List;
+
 import interfacedao.maestros.IF40205DAO;
+
+import modelo.maestros.F40205;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,28 @@ public class SF40205 {
 
 	@Autowired
 	private IF40205DAO f40205DAO;
+
+	public void guardar(F40205 f40) {
+		f40205DAO.save(f40);
+	}
+
+	public List<F40205> buscarTodosOrdenados() {
+		return f40205DAO.findAllOrderByAban8dAsc();
+	}
+
+	public void eliminarVarios(List<F40205> eliminarLista) {
+		f40205DAO.delete(eliminarLista);
+	}
+
+	public void eliminarUno(String clave) {
+		f40205DAO.delete(clave);
+	}
+
+	public Object buscar(String value) {
+		return f40205DAO.findOne(value);
+	}
+
+	public boolean comparar(String value) {
+		return f40205DAO.exists(value);
+	}
 }
