002ef685f9a3090e7646cd69ba5e26dedd44c1bb
linkin9014 <linkin9014@gmail.com>
2014-10-09 18:02:16 +0000

    Mejorada la funcionalidad del reporte de existencia, basandose en las necesidades del user Mejorado el catalogo de items de las transacciones

--- Bizapps-Inventario/src/controlador/reporte/CRAlmacen.java
+++ Bizapps-Inventario/src/controlador/reporte/CRAlmacen.java
@@ -8,10 +8,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import modelo.maestros.F4101;
+import modelo.maestros.F41021;
 import modelo.maestros.F4105;
 import modelo.pk.F4105PK;
 import modelo.transacciones.F4111;
@@ -24,13 +26,13 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Tab;
 
 import componentes.Botonera;
 import componentes.Mensaje;
-
 import controlador.maestros.CGenerico;
 
 public class CRAlmacen extends CGenerico {
@@ -41,6 +43,8 @@ public class CRAlmacen extends CGenerico {
 	private Div botoneraVAlmacen;
 	@Wire
 	private Datebox dtbFecha;
+	@Wire
+	private Checkbox chkTodos;
 	private static final long serialVersionUID = 3154539496606898070L;
 
 	@Override
@@ -72,7 +76,17 @@ public class CRAlmacen extends CGenerico {
 				BigDecimal hasta = transformarGregorianoAJulia(dtbFecha
 						.getValue());
 				// List<F4111> ordenes = servicioF4111.buscarHastaFecha(hasta);
-				List<F4111> ordenes = servicioF4111.buscarHastaFecha2(hasta);
+				// List<F4111> ordenes = servicioF4111.buscarHastaFecha2(hasta);
+				List<F41021> ordenes = new ArrayList<F41021>();
+				String todos = "";
+				if (chkTodos.isChecked()) {
+					ordenes = getServicioF41021().buscarHastaFecha2(hasta);
+					todos = "si";
+				} else {
+					ordenes = getServicioF41021().buscarHastaFechaExistencia2(
+							hasta);
+					todos = "no";
+				}
 				if (!ordenes.isEmpty()) {
 					DateFormat fecha = new SimpleDateFormat("dd-MM-yyyy");
 					String fecha11 = fecha.format(dtbFecha.getValue());
@@ -80,6 +94,8 @@ public class CRAlmacen extends CGenerico {
 							+ damePath()
 							+ "Generador?valor=2&valor2="
 							+ fecha11
+							+ "&valor4="
+							+ todos
 							+ "','','top=100,left=200,height=600,width=800,scrollbars=1,resizable=1')");
 				} else
 					msj.mensajeAlerta(Mensaje.noHayRegistros);
@@ -88,6 +104,7 @@ public class CRAlmacen extends CGenerico {
 			@Override
 			public void limpiar() {
 				dtbFecha.setValue(fecha);
+				chkTodos.setChecked(false);
 			}
 
 			@Override
@@ -129,7 +146,101 @@ public class CRAlmacen extends CGenerico {
 		botoneraVAlmacen.appendChild(botonera);
 	}
 
-	public byte[] reporte(String part2) {
+	// public byte[] reporte(String part2) {
+	// byte[] fichero = null;
+	// SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+	// Date fecha1 = null;
+	// try {
+	// fecha1 = formato.parse(part2);
+	// } catch (ParseException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// BigDecimal desde = transformarGregorianoAJulia(fecha1);
+	// // List<F4111> ordenes = getServicioF4111().buscarHastaFecha(desde);
+	// List<F4111> ordenes = getServicioF4111().buscarHastaFecha2(desde);
+	// List<F4111> finales = new ArrayList<F4111>();
+	// // Date fechaMa =
+	// // transformarJulianaAGregoria(ordenes.get(0).getIlcrdj());
+	// Date fechaMa = transformarJulianaAGregoria(ordenes.get(0).getIlvpej());
+	// ordenes.get(0).setIlasid(formatoFecha.format(fechaMa));
+	// F4101 f = new F4101();
+	// if (ordenes.get(0).getIlitm() != null) {
+	// f = getServicioF4101().buscar(ordenes.get(0).getIlitm());
+	// ordenes.get(0).setIltrex(f.getImdsc1());
+	// ordenes.get(0).setIlplot(f.getImuom1());
+	// } else {
+	// ordenes.get(0).setIlplot("");
+	// ordenes.get(0).setIltrex("");
+	// }
+	// finales.add(ordenes.get(0));
+	// double valor = ordenes.get(0).getIlitm();
+	// String mcu = ordenes.get(0).getIlmcu();
+	// String lcon2 = ordenes.get(0).getIllocn();
+	// for (int i = 0; i < ordenes.size(); i++) {
+	// Date fechaM = transformarJulianaAGregoria(ordenes.get(i)
+	// .getIlvpej());
+	// ordenes.get(i).setIlasid(formatoFecha.format(fechaM));
+	// f = new F4101();
+	// if (ordenes.get(i).getIlitm() != null) {
+	// f = getServicioF4101().buscar(ordenes.get(i).getIlitm());
+	// ordenes.get(i).setIltrex(f.getImdsc1());
+	// ordenes.get(i).setIlplot(f.getImuom1());
+	// } else {
+	// ordenes.get(i).setIlplot("");
+	// ordenes.get(i).setIltrex("");
+	// }
+	// if (ordenes.get(i).getIlitm() != valor) {
+	// finales.add(ordenes.get(i));
+	// valor = ordenes.get(i).getIlitm();
+	// mcu = ordenes.get(i).getIlmcu();
+	// lcon2 = ordenes.get(0).getIllocn();
+	// }
+	// }
+	//
+	// F4105 costo = new F4105();
+	// F4105PK claveCostoUnitario = new F4105PK();
+	//
+	// for (int i = 0; i < finales.size(); i++) {
+	// costo = new F4105();
+	// claveCostoUnitario = new F4105PK();
+	// double item = finales.get(i).getIlitm();
+	// String mcu2 = finales.get(i).getIlmcu();
+	// String locn = finales.get(i).getIllocn();
+	// claveCostoUnitario.setCoitm(item);
+	// claveCostoUnitario.setComcu(mcu2);
+	// claveCostoUnitario.setColocn(locn);
+	// claveCostoUnitario.setColotn("");
+	// claveCostoUnitario.setColedg("");
+	// costo = getServicioF4105().buscar(claveCostoUnitario);
+	// Double costoUnitario = (double) 0;
+	// if (costo != null)
+	// costoUnitario = costo.getCouncs();
+	// Double cantidad = getServicioF4111().sumar(item, mcu2, locn);
+	// finales.get(i).setIluncs(costoUnitario);
+	// finales.get(i).setIltrqt(cantidad);
+	// Double total = costoUnitario * cantidad;
+	// finales.get(i).setIlpaid(total);
+	// }
+	// Map p = new HashMap();
+	// p.put("fecha", fecha1);
+	// JasperReport repor = null;
+	// try {
+	// repor = (JasperReport) JRLoader.loadObject(getClass().getResource(
+	// "/reporte/RAlmacen.jasper"));
+	// } catch (JRException e1) {
+	// e1.printStackTrace();
+	// }
+	// try {
+	// fichero = JasperRunManager.runReportToPdf(repor, p,
+	// new JRBeanCollectionDataSource(finales));
+	// } catch (JRException e) {
+	// msj.mensajeError(Mensaje.errorEnReporte);
+	// }
+	// return fichero;
+	// }
+
+	public byte[] reporte2(String part2, String part4) {
 		byte[] fichero = null;
 		SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
 		Date fecha1 = null;
@@ -140,70 +251,39 @@ public class CRAlmacen extends CGenerico {
 			e.printStackTrace();
 		}
 		BigDecimal desde = transformarGregorianoAJulia(fecha1);
-		// List<F4111> ordenes = getServicioF4111().buscarHastaFecha(desde);
-		List<F4111> ordenes = getServicioF4111().buscarHastaFecha2(desde);
-		List<F4111> finales = new ArrayList<F4111>();
-		// Date fechaMa =
-		// transformarJulianaAGregoria(ordenes.get(0).getIlcrdj());
-		Date fechaMa = transformarJulianaAGregoria(ordenes.get(0).getIlvpej());
-		ordenes.get(0).setIlasid(formatoFecha.format(fechaMa));
-		F4101 f = new F4101();
-		if (ordenes.get(0).getIlitm() != null) {
-			f = getServicioF4101().buscar(ordenes.get(0).getIlitm());
-			ordenes.get(0).setIltrex(f.getImdsc1());
-			ordenes.get(0).setIlplot(f.getImuom1());
+		List<F41021> ordenes = new ArrayList<F41021>();
+//		List<F41021> ordenes = getServicioF41021().buscarHastaFecha2(desde);
+		if (part4.equals("si")) {
+			ordenes = getServicioF41021().buscarHastaFecha2(desde);
 		} else {
-			ordenes.get(0).setIlplot("");
-			ordenes.get(0).setIltrex("");
+			ordenes = getServicioF41021().buscarHastaFechaExistencia2(
+					desde);
 		}
-		finales.add(ordenes.get(0));
-		double valor = ordenes.get(0).getIlitm();
-		String mcu = ordenes.get(0).getIlmcu();
-		String lcon2 = ordenes.get(0).getIllocn();
-		for (int i = 0; i < ordenes.size(); i++) {
-			Date fechaM = transformarJulianaAGregoria(ordenes.get(i)
-					.getIlvpej());
-			ordenes.get(i).setIlasid(formatoFecha.format(fechaM));
-			f = new F4101();
-			if (ordenes.get(i).getIlitm() != null) {
-				f = getServicioF4101().buscar(ordenes.get(i).getIlitm());
-				ordenes.get(i).setIltrex(f.getImdsc1());
-				ordenes.get(i).setIlplot(f.getImuom1());
-			} else {
-				ordenes.get(i).setIlplot("");
-				ordenes.get(i).setIltrex("");
-			}
-			if (ordenes.get(i).getIlitm() != valor) {
-				finales.add(ordenes.get(i));
-				valor = ordenes.get(i).getIlitm();
-				mcu = ordenes.get(i).getIlmcu();
-				lcon2 = ordenes.get(0).getIllocn();
-			}
-		}
-
-		F4105 costo = new F4105();
-		F4105PK claveCostoUnitario = new F4105PK();
-
-		for (int i = 0; i < finales.size(); i++) {
-			costo = new F4105();
-			claveCostoUnitario = new F4105PK();
-			double item = finales.get(i).getIlitm();
-			String mcu2 = finales.get(i).getIlmcu();
-			String locn = finales.get(i).getIllocn();
+		Date fechaNew = transformarJulianaAGregoria(desde);
+		String fechaReporte = formatoFecha.format(fechaNew);
+		List<F41021> ordenesFinales = new ArrayList<F41021>();
+		for (Iterator<F41021> iterator = ordenes.iterator(); iterator.hasNext();) {
+			F41021 f41021 = (F41021) iterator.next();
+			F4105 costo = new F4105();
+			F4105PK claveCostoUnitario = new F4105PK();
+			double item = f41021.getId().getLiitm();
+			String mcu2 = f41021.getId().getLimcu();
 			claveCostoUnitario.setCoitm(item);
 			claveCostoUnitario.setComcu(mcu2);
-			claveCostoUnitario.setColocn(locn);
+			claveCostoUnitario.setColocn("");
 			claveCostoUnitario.setColotn("");
-			claveCostoUnitario.setColedg("");
+			claveCostoUnitario.setColedg("02");
 			costo = getServicioF4105().buscar(claveCostoUnitario);
 			Double costoUnitario = (double) 0;
 			if (costo != null)
 				costoUnitario = costo.getCouncs();
-			Double cantidad = getServicioF4111().sumar(item, mcu2, locn);
-			finales.get(i).setIluncs(costoUnitario);
-			finales.get(i).setIltrqt(cantidad);
-			Double total = costoUnitario * cantidad;
-			finales.get(i).setIlpaid(total);
+			F4101 f = getServicioF4101().buscar(f41021.getId().getLiitm());
+			f41021.setLicfgsid(fechaReporte);
+			f41021.setLisqwo(costoUnitario);
+			f41021.setLisreq(costoUnitario * f41021.getLipqoh());
+			f41021.setLichdf(f.getImdsc1());
+			f41021.setLiglpt(f.getImuom1());
+			ordenesFinales.add(f41021);
 		}
 		Map p = new HashMap();
 		p.put("fecha", fecha1);
@@ -216,7 +296,7 @@ public class CRAlmacen extends CGenerico {
 		}
 		try {
 			fichero = JasperRunManager.runReportToPdf(repor, p,
-					new JRBeanCollectionDataSource(finales));
+					new JRBeanCollectionDataSource(ordenesFinales));
 		} catch (JRException e) {
 			msj.mensajeError(Mensaje.errorEnReporte);
 		}
--- Bizapps-Inventario/src/interfacedao/maestros/IF41021DAO.java
+++ Bizapps-Inventario/src/interfacedao/maestros/IF41021DAO.java
@@ -1,5 +1,6 @@
 package interfacedao.maestros;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import modelo.maestros.F41021;
@@ -12,5 +13,11 @@ public interface IF41021DAO extends JpaRepository<F41021, F41021PK> {
 
 	@Query("select distinct f.id.liitm from F41021 f where f.id.limcu = ?1")
 	List<Double> buscarItemDistintos(String value);
+
+	@Query("select f from F41021 f where f.liupmj <= ?1 order by f.id.limcu asc, f.id.lilocn asc, f.id.liitm asc")
+	List<F41021> findByLiupmjBeforeNow(BigDecimal desde);
+
+	@Query("select f from F41021 f where f.liupmj <= ?1 and f.lipqoh <> 0 order by f.id.limcu asc, f.id.lilocn asc, f.id.liitm asc")
+	List<F41021> findByLiupmjBeforeNowExistencia(BigDecimal hasta);
 
 }
