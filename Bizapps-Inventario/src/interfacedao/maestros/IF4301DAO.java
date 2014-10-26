99ef20b882df9dccc674c135f175a91b8ec36570
thejorgemylio <thejorgemylio@gmail.com>
2014-10-26 17:29:18 +0000

    Cabecera de Orden de Compra Normal

---
+++ Bizapps-Inventario/src/interfacedao/maestros/IF4301DAO.java
@@ -0,0 +1,16 @@
+package interfacedao.maestros;
+
+import java.util.List;
+
+import modelo.maestros.F4301;
+import modelo.pk.F4301PK;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface IF4301DAO extends JpaRepository<F4301, F4301PK> {
+
+	@Query("Select f from F4301 f order by f.id.phdoco asc")
+	List<F4301> findAllOrderByIdPhdoco();
+
+}
---
+++ Bizapps-Inventario/src/servicio/maestros/SF4301.java
@@ -0,0 +1,27 @@
+package servicio.maestros;
+
+import interfacedao.maestros.IF4301DAO;
+
+import java.util.List;
+
+import modelo.maestros.F4301;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("SF4301")
+public class SF4301 {
+
+	@Autowired
+	private IF4301DAO iF4301DAO;
+
+	public void guardar(F4301 f4301) {
+		System.out.println(f4301.toString());
+		iF4301DAO.save(f4301);
+	}
+
+	public List<F4301> buscarTodosOrdenados() {
+		return iF4301DAO.findAllOrderByIdPhdoco();
+	}
+
+}
